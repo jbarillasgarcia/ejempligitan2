900a321eb93d08717c7067e518597f4b4662237b
joshi <joshi@SCAR2>
2019-09-26 20:08:25 -0600

    Esta es la primera version de mi programa.

---
+++ UMGPlay/src/main/java/gt/edu/umg/ingenieria/sistemas/an2/umgplay/UMGPlayApp.java
@@ -0,0 +1,9 @@
+package gt.edu.umg.ingenieria.sistemas.an2.umgplay;
+
+public class UMGPlayApp {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!!!");
+    }
+    
+}
