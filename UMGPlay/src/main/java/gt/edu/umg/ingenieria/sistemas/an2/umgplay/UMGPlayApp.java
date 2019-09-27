53e778f8345982d49c3033339e2e833b991d8ddf
joshi <joshi@SCAR2>
2019-09-26 20:17:55 -0600

    Modifiqué el mensaje porque no me gustaba el original.

--- UMGPlay/src/main/java/gt/edu/umg/ingenieria/sistemas/an2/umgplay/UMGPlayApp.java
+++ UMGPlay/src/main/java/gt/edu/umg/ingenieria/sistemas/an2/umgplay/UMGPlayApp.java
@@ -3,7 +3,7 @@ package gt.edu.umg.ingenieria.sistemas.an2.umgplay;
 public class UMGPlayApp {
     
     public static void main(String[] args) {
-        System.out.println("Hola mundo!!!");
+        System.out.println("Hola mundo!!! (Este texto ha sido modificado)");
     }
     
 }
